c46ab0a7d92ad52089245000509f965dc40c3f0a
Liam <40437578@live.napier.ac.uk>
2024-07-18 21:53:43 +0100

    Added Needle Rounds Item and Updated All Item Sprites
    - Added needle rounds, an uncommon item that increases critical strike chance and damage.
    - Remade all the item icon images, using the in-game renderer instead of the Unity editor as its more representative of how each item looks in game.

---
+++ Items/NeedleRounds.cs
@@ -0,0 +1,228 @@
+﻿using System.Runtime.CompilerServices;
+using R2API;
+using R2API.Utils;
+using RoR2;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.Diagnostics;
+using UnityEngine.UIElements;
+using static MoreItems.MoreItems;
+
+namespace MoreItems.Items
+{
+    /// <summary>
+    /// NeedleRounds - T2 (Uncommon) Item
+    /// <para>Increases critical strike damage slightly.</para>
+    /// <para>If critical strike chance is less than 10%, it will also be increased to 10%.</para>
+    /// </summary>
+    public class NeedleRounds : Item
+    {
+        public override string Name => "Needle Rounds";
+        public override string NameToken => "NEEDLEROUNDS";
+        public override string PickupToken => "Increases critical strike chance and damage slightly.";
+        public override string Description => "Gain <style=cIsDamage>+10%</style><style=cStack> (+10% per stack)</style> increased <style=cIsDamage>critical strike chance and damage</style>.";
+        public override string Lore => "<style=cMono>// INTERCEPTED RADIO TRANSMISSIONS. //\n// PRINTING TRANSCRIPT... //</style>\n\nExperimental munitions MUST be banned today!\n\nSmall arms research has gone too far, and these so called 'Plated Rounds' harbour a sinister truth. They are designed to kill with brutal efficiency.\n\nShaped charges built into the tail end of the bullet detonate when the bullet has pierced it's target, sending forth huge bursts of shrapnel that decimate the target from the inside!\n\nWar is inevitable, but it does not always need to be a bloodbath. Incapacitation or disarming is always preferred, the less soldiers killed in battle, the better. And we already have methods to ensure that. Private military research into barbaric technologies like these prove that they are only interested in death without resolution.\n\nWe the people demand a system-wide ban on these munitions, and an immediate stop to any further research into similar technologies. For the sake of humanity. <style=cMono>\n\n// TRANSCRIPT FINISHED. //</style>";
+
+        public override ItemTier Tier => ItemTier.Tier2;
+
+        public override bool CanRemove => true;
+
+        public override ItemTag[] Tags => new ItemTag[] { ItemTag.Damage };
+        public override bool AIBlackList => false;
+
+        public override Sprite Icon => null;
+        public override GameObject Model => MainAssets.LoadAsset<GameObject>("NeedleRounds.prefab");
+
+        public override void SetupHooks()
+        {
+            R2API.RecalculateStatsAPI.GetStatCoefficients += (self, args) =>
+            {
+                if(!self || !self.inventory || self.inventory.GetItemCount(itemDef) <= 0) { return; }
+
+                args.critAdd += 10f * self.inventory.GetItemCount(itemDef);
+                args.critDamageMultAdd += 0.10f * self.inventory.GetItemCount(itemDef);
+            };
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            ItemDisplayRuleDict rules = new ItemDisplayRuleDict();
+
+            GameObject display = MainAssets.LoadAsset<GameObject>("BountyHunterBadge.prefab");
+
+            var itemDisplay = display.AddComponent<ItemDisplay>();
+            itemDisplay.rendererInfos = ItemDisplaySetup(display);
+
+            rules.Add("mdlCommandoDualies", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0.06313F, 0.22037F, 0.09908F),
+                    localAngles = new Vector3(45.34884F, 106.977F, 165.8645F),
+                    localScale = new Vector3(0F, 0F, 0F)
+                }
+            });
+
+            rules.Add("mdlHuntress", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlBandit2", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlToolbot", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlEngi", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "UpperArmL",
+                    localPos = new Vector3(0.09373F, 0.03316F, 0.01377F),
+                    localAngles = new Vector3(28.99673F, 162.3914F, 172.891F),
+                    localScale = new Vector3(0F, 0F, 0F)
+                }
+            });
+
+            rules.Add("mdlMage", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighR",
+                    localPos = new Vector3(-0.08462F, 0.36293F, 0.1016F),
+                    localAngles = new Vector3(37.80873F, 12.43991F, 179.8899F),
+                    localScale = new Vector3(0F, 0F, 0F)
+                }
+            });
+
+            rules.Add("mdlMerc", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighR",
+                    localPos = new Vector3(-0.13209F, 0.20849F, -0.02851F),
+                    localAngles = new Vector3(43.4352F, 350.5829F, 195.3876F),
+                    localScale = new Vector3(0F, 0F, 0F)
+                }
+            });
+
+            rules.Add("mdlTreebot", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "Base",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlLoader", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlCroco", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlCaptain", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlRailGunner", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            rules.Add("mdlVoidSurvivor", new RoR2.ItemDisplayRule[]
+            {
+                new RoR2.ItemDisplayRule
+                {
+                    ruleType = ItemDisplayRuleType.ParentedPrefab,
+                    followerPrefab = display,
+                    childName = "ThighL",
+                    localPos = new Vector3(0f, 0f, 0f),
+                    localAngles = new Vector3(0f, 0f, 0f),
+                    localScale = new Vector3(0f, 0f, 0f)
+                }
+            });
+
+            return rules;
+        }
+    }
+}
